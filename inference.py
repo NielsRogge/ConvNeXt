dcd4fcd87a257f21a661d3a3b03af45717e99983
Niels Rogge <niels.rogge1@gmail.com>
2022-02-02 18:42:15 +0100

    Fix normalize

--- inference.py
+++ inference.py
@@ -25,6 +25,7 @@ from timm.data.mixup import Mixup
 from timm.models import create_model
 from timm.loss import LabelSmoothingCrossEntropy, SoftTargetCrossEntropy
 from timm.utils import ModelEma
+from timm.data.constants import IMAGENET_DEFAULT_MEAN, IMAGENET_DEFAULT_STD
 from optim_factory import create_optimizer, LayerDecayValueAssigner
 
 from datasets import build_dataset
@@ -254,7 +255,8 @@ def main(args):
     transformations = transforms.Compose(
         [transforms.Resize(args.input_size, args.input_size),
          transforms.ToTensor(),
-         transforms.Normalize())]
+         transforms.Normalize(mean=IMAGENET_DEFAULT_MEAN, std=IMAGENET_DEFAULT_STD)
+        ]
     )
     
     pixel_values = transformations(image).unsqueeze(0)
